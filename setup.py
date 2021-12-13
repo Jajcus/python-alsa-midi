452698a88b2e562879089012df9192a81767b5e4
Jacek Konieczny <jajcus@jajcus.net>
2021-12-13 22:28:05 +0100

    do not compile cffi module in Python 3.10

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+import sys
+
 from setuptools import setup
 
+if sys.version_info < (3, 10):
+    cffi_modules = ["alsa_midi/_ffi_defs.py:ffi"]
+else:
+    cffi_modules = None
+
 setup(
-    cffi_modules=["alsa_midi/_ffi_defs.py:ffi"]
+    cffi_modules=cffi_modules
     )
