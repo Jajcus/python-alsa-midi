ae1556c6659d7c1a77783b33cb387b716dba8f1d
Jacek Konieczny <jajcus@jajcus.net>
2021-12-13 22:31:31 +0100

    Revert "do not compile cffi module in Python 3.10"
    This reverts commit 452698a88b2e562879089012df9192a81767b5e4.

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
-import sys
-
 from setuptools import setup
 
-if sys.version_info < (3, 10):
-    cffi_modules = ["alsa_midi/_ffi_defs.py:ffi"]
-else:
-    cffi_modules = None
-
 setup(
-    cffi_modules=cffi_modules
+    cffi_modules=["alsa_midi/_ffi_defs.py:ffi"]
     )
