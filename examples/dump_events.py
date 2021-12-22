fe34654ad0bda4a2d4ce27aa17ae0c164a76446d
Jacek Konieczny <jajcus@jajcus.net>
2021-12-22 10:35:41 +0100

    examples: dump_events: real-time timestamping added

--- examples/dump_events.py
+++ examples/dump_events.py
@@ -7,13 +7,24 @@ from alsa_midi import WRITE_PORT, Address, SequencerClient
 
 def main():
     parser = ArgumentParser(description="Dump / sniff ALSA sequencer events")
+    parser.add_argument("--real-time", action="store_true", default=None,
+                        help="Force timestamps in seconds")
     parser.add_argument("--port", "-p", type=Address, action="append",
                         help="Connect to the specific port")
 
     args = parser.parse_args()
 
     client = SequencerClient("dump_events.py")
-    port = client.create_port("input", WRITE_PORT)
+
+    if args.real_time:
+        queue = client.create_queue()
+    else:
+        queue = None
+
+    port = client.create_port("input", WRITE_PORT,
+                              timestamping=args.real_time,
+                              timestamp_real=args.real_time,
+                              timestamp_queue=queue)
     addr = Address(port)
 
     print(f"Listening for events at sequecncer port {addr}")
@@ -22,10 +33,25 @@ def main():
         for target_port in args.port:
             port.connect_from(target_port)
 
+    if queue is not None:
+        queue.start()
+        client.drain_output()
+
+    time_h, source_h, event_h = "Time", "Source", "Event"
+    print(f"{time_h:>15} {source_h:7} {event_h}")
     try:
         while True:
             event = client.event_input()
-            print(repr(event))
+            if event is None:
+                print("no event")
+                continue
+            if event.time is not None:
+                time_s = str(event.time)
+            elif event.tick is not None:
+                time_s = str(event.tick)
+            else:
+                time_s = ""
+            print(f"{time_s:>15} {event.source!s:7} {event!r}")
     except KeyboardInterrupt:
         pass
 
