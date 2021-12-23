662084717c7bc0f0a4737f4f8b4012218efb0327
Jacek Konieczny <jajcus@jajcus.net>
2021-12-23 15:04:31 +0100

    make all event classes importable from alsa_midi

--- alsa_midi/__init__.py
+++ alsa_midi/__init__.py
@@ -3,8 +3,19 @@
 from ._ffi import alsa, ffi
 from .address import ALL_SUBSCRIBERS, SYSTEM_ANNOUNCE, SYSTEM_TIMER, Address
 from .client import AsyncSequencerClient, ClientInfo, ClientType, SequencerClient
-from .event import (Event, EventFlags, EventType, MidiBytesEvent, NoteEventBase, NoteOffEvent,
-                    NoteOnEvent, RealTime)
+from .event import (ActiveSensingEvent, BounceEvent, ChannelPressureEvent, ClientChangeEvent,
+                    ClientExitEvent, ClientStartEvent, ClockEvent, ContinueEvent,
+                    Control14BitChangeEvent, ControlChangeEvent, EchoEvent, Event, EventFlags,
+                    EventType, KeyPressEvent, KeySignatureEvent, MidiBytesEvent,
+                    NonRegisteredParameterChangeEvent, NoteEvent, NoteOffEvent, NoteOnEvent,
+                    OSSEvent, PitchBendEvent, PortChangeEvent, PortExitEvent, PortStartEvent,
+                    PortSubscribedEvent, PortUnsubscribedEvent, ProgramChangeEvent, QueueSkewEvent,
+                    RealTime, RegisteredParameterChangeEvent, ResetEvent, ResultEvent,
+                    SetQueuePositionTickEvent, SetQueuePositionTimeEvent, SetQueueTempoEvent,
+                    SongPositionPointerEvent, SongSelectEvent, StartEvent, StopEvent,
+                    SyncPositionChangedEvent, SysExEvent, SystemEvent, TickEvent,
+                    TimeSignatureEvent, TuneRequestEvent, UserVar0Event, UserVar1Event,
+                    UserVar2Event, UserVar3Event)
 from .exceptions import ALSAError, Error, StateError
 from .port import READ_PORT, RW_PORT, WRITE_PORT, Port, PortCaps, PortInfo, PortType
 from .queue import Queue
@@ -13,10 +24,22 @@ __all__ = [
         "Address", "ALL_SUBSCRIBERS", "SYSTEM_TIMER", "SYSTEM_ANNOUNCE",
         "SequencerClient", "AsyncSequencerClient", "ClientInfo", "ClientType",
         "RealTime", "EventType", "EventFlags", "Event", "MidiBytesEvent",
-        "NoteEventBase", "NoteOnEvent", "NoteOffEvent",
         "Error", "StateError", "ALSAError",
         "Port", "PortCaps", "PortType", "PortInfo",
         "READ_PORT", "WRITE_PORT", "RW_PORT",
         "Queue",
         "alsa", "ffi",
+
+        "SystemEvent", "ResultEvent", "NoteEvent", "NoteOnEvent", "NoteOffEvent", "KeyPressEvent",
+        "ControlChangeEvent", "ProgramChangeEvent", "ChannelPressureEvent", "PitchBendEvent",
+        "Control14BitChangeEvent", "NonRegisteredParameterChangeEvent",
+        "RegisteredParameterChangeEvent", "SongPositionPointerEvent", "SongSelectEvent",
+        "TimeSignatureEvent", "KeySignatureEvent", "StartEvent", "ContinueEvent", "StopEvent",
+        "SetQueuePositionTickEvent", "SetQueuePositionTimeEvent", "SetQueueTempoEvent",
+        "ClockEvent", "TickEvent", "QueueSkewEvent", "SyncPositionChangedEvent",
+        "TuneRequestEvent", "ResetEvent", "ActiveSensingEvent", "EchoEvent", "OSSEvent",
+        "ClientStartEvent", "ClientExitEvent", "ClientChangeEvent", "PortStartEvent",
+        "PortExitEvent", "PortChangeEvent", "PortSubscribedEvent", "PortUnsubscribedEvent",
+        "SysExEvent", "BounceEvent", "UserVar0Event", "UserVar1Event", "UserVar2Event",
+        "UserVar3Event",
         ]
